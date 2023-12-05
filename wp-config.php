9bcc13a2d5ae0e508de4adcee6c9aac4ea3bac24
真_人工智障 <67090055+TotoWang-hhh@users.noreply.github.com>
2023-12-05 23:44:24 +0800

    Add files via upload

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'epiz_33244859_wp' );
+define( 'DB_NAME', 'epiz_28245616_blog' );
 
 /** MySQL database username */
-define( 'DB_USER', 'epiz_33244859' );
+define( 'DB_USER', 'epiz_28245616' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '948tt637908' );
+define( 'DB_PASSWORD', 'Wtt637908' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'sql108.epizy.com' );
+define( 'DB_HOST', 'sql100.epizy.com' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
