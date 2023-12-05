9635b1004fdd27808369f787d4dfce2eae288f3c
真_人工智障 <67090055+TotoWang-hhh@users.noreply.github.com>
2023-12-05 22:43:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
+$file= __DIR__ . '/..'.$_SERVER["PHP_SELF"];
+
+if(file_exists($file))
+{
+   return false;
+}
+else
+{
+    require_once __DIR__ . '/../index.php';
+}
+#echo $_SERVER["PHP_SELF"];
+
+
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
@@ -12,19 +25,6 @@
  * @var bool
  */
 define( 'WP_USE_THEMES', true );
-
-<?php
-$file= __DIR__ . '/..'.$_SERVER["PHP_SELF"];
-
-if(file_exists($file))
-{
-   return false;
-}
-else
-{
-    require_once __DIR__ . '/../index.php';
-}
-#echo $_SERVER["PHP_SELF"];
 
 /** Loads the WordPress Environment and Template */
 require __DIR__ . '/wp-blog-header.php';
